4ead2a98c1afb1da1da0136b77f2f691a6899a5a
kondarao056 <43640445+kondarao056@users.noreply.github.com>
2024-08-31 15:30:30 +0530

    project commiting

--- database.sh
+++ database.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+FILEPATH="/var/log/mysql_logs"
+SCRIPT=$(echo $0 | cut -d "." -1f)
+TIME_STAMP=$(date +%D)
+LOG_FILE=$SCRIPT$TIME_STAMP$FILEPATH.log
+mkdir -p mysql_logs
+
 USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
@@ -9,19 +15,19 @@ ROOT()
 {
     if [ $USERID -ne 0 ]
     then
-        echo -e "$R YOU ARE NOT IN ROOT PREVILIGES PLEASE RUN WITH SUDO $N"
+        echo -e "$R YOU ARE NOT IN ROOT PREVILIGES PLEASE RUN WITH SUDO $N" | tee -a $LOG_FILE
         exit 1
     else
-        echo -e "$G SERVER STARTED FOR EXECUTING SERVICES $N"
+        echo -e "$G SERVER STARTED FOR EXECUTING SERVICES $N" | tee -a $LOG_FILE
     fi
 }
 VALIDATE() {
     if [ $1 -ne 0 ]
     then 
-        echo "$2 is : FAILED"
+        echo "$2 is : FAILED" | tee -a $LOG_FILE
         exit 1
     else
-        echo "$2 is : SUCCESS"
+        echo "$2 is : SUCCESS" | tee -a $LOG_FILE
     fi
 }
 
@@ -30,17 +36,17 @@ ROOT
 dnf list installed mysql-server;
 if [ $? -ne 0 ]
 then
-    echo "MYSQL SERVER NOT INSTALLED..GOING TO INSTALLING"
-    dnf install mysql-server -y
+    echo "MYSQL SERVER NOT INSTALLED..GOING TO INSTALLING" | tee -a $LOG_FILE
+    dnf install mysql-server -y | & >>$LOG_FILE
       if [ $? -ne 0 ]
       then
-            echo -e "$R MYSQL SERVER NOT INSTALLED PROPERLLY RETRY $N"
+            echo -e "$R MYSQL SERVER NOT INSTALLED PROPERLLY RETRY $N" | tee -a $LOG_FILE
         exit 1
        else
-            echo -e "$G SQL_SERVER INSTALLED SUCCESSFULLY $N"
+            echo -e "$G SQL_SERVER INSTALLED SUCCESSFULLY $N" | tee -a $LOG_FILE
       fi
 else
-    echo -e "$Y MYSQL_SERVER PACKEGES ARE ALREADY AVAILABLE $N";
+    echo -e "$Y MYSQL_SERVER PACKEGES ARE ALREADY AVAILABLE $N"; | tee -a $LOG_FILE
 fi
 
 systemctl enable mysqld
