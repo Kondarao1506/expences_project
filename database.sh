dead982424464bb98db1c8321e85a3f2876661e8
kondarao056 <43640445+kondarao056@users.noreply.github.com>
2024-08-31 14:27:54 +0530

    project commiting

--- database.sh
+++ database.sh
@@ -6,11 +6,11 @@ VALIDATE() {
         echo "$2 is : FAILED"
         exit 1
     else
-        echo "$2 is : SUCCESS
+        echo "$2 is : SUCCESS"
     fi
 }
 
-dnf list installed mysql-server
+dnf list installed mysql-server;
 if [ $? -ne 0 ]
 then
     echo "MYSQL SERVER NOT INSTALLED..GOING TO INSTALLING"
@@ -23,14 +23,13 @@ then
           echo "MYSQL_SERVER INSTALLED SUCCESSFULLY"
       fi
 else
-    echo "MYSQL_SERVER PACKEGES ARE ALREADY AVAILABLE"
+    echo "MYSQL_SERVER PACKEGES ARE ALREADY AVAILABLE";
 fi
 
 systemctl enable mysqld
 if [ $? -ne 0]
 then 
     echo "mysql server going to enable "
-    systemctl start mysqld
+    systemctl start mysqldf    
     VALIDATE() $? "MYSQL_SERVER"
 fi
-
